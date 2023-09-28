da39a25b23d77b3bd6479dec7af858c221597b89
je-2014 <144688008+je-2014@users.noreply.github.com>
2023-09-28 09:50:39 +0100

    Delete week 3 exercise/Time directory

--- week 3 exercise/Time/time.js
+++
@@ -1,6 +0,0 @@
-var date = new Date();
-var day = date.getDate();
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-
-alert(""+day+"/"+month+"/"+year);
--- week 3 exercise/Time/triangle.js
+++
@@ -1,7 +0,0 @@
-var side1 = 5;
-var side2 = 6;
-var side3 = 7;
-
-var s = (side1+side2+side3)/2;
-
-document.getElementById("demo").innerHTML = s;
